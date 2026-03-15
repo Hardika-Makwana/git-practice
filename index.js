9a8f7c713a92f7aab655e817b0e6d99e46797ba9
Hardika-Makwana <166051037+Hardika-Makwana@users.noreply.github.com>
2026-03-15 09:08:59 +0530

    added age func and chnage name to name1

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const age = 24;
-const name = "John Doe";
+const name1 = "John Doe";
 const dob = "01/01/2000";
 function sayName(){
-    console.log(`My name is ${name}`);
+    console.log(`My name is ${name1}`);
+}
+function above18(){
+    return age > 18;
 }
 
