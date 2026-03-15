61197d97da39782f0798c3686610411e669216ba
Hardika-Makwana <166051037+Hardika-Makwana@users.noreply.github.com>
2026-03-15 22:22:28 +0530

    revert func removal

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const dob = "01/01/2000";
 function sayName(){
     console.log(`My name is ${name1}`);
 }
+<<<<<<< HEAD
 function newfn(){
     console.log("This is a new function");
 }
+=======
+function above18(){
+    return age > 18;
+}
+
+>>>>>>> parent of 34ca7c3 (remove age func)
